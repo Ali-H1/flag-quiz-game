a35f17c03d2d16dfd79ef651d7564f334e74e347
Ali.H1 <ali.haddadi154@gmail.com>
2021-05-25 14:46:30 +0430

    Added timer

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+var timer = 1;
+var j = 0;
+
 const app = Vue.createApp({
   data() {
     return {
@@ -49,6 +52,8 @@ const app = Vue.createApp({
       this.displayoptions = "block";
       this.clicked = false;
       this.resultstyle = ["", "", "", ""];
+      timer = 1;
+      this.progressBar();
     },
     answer() {
       var opt1 = document.getElementById("opt1txt").innerHTML;
@@ -56,15 +61,47 @@ const app = Vue.createApp({
       var opt3 = document.getElementById("opt3txt").innerHTML;
       var opt4 = document.getElementById("opt4txt").innerHTML;
       var opts = [opt1, opt2, opt3, opt4];
-      var index = -1;
+      var indexopt = -1;
       for (i = 0; i < 3; i++) {
-        if (opts[i] == this.correctopt) {
-          index = i;
+        if (opts[i] === this.correctopt) {
+          indexopt = i;
         }
       }
-      this.resultstyle[index] = "rgb(5, 151, 5)";
+      this.resultstyle[indexopt] = "rgb(5, 151, 5)";
     },
-    livesleft(){
+    progressBar() {
+      if (j == 0) {
+        j = 1;
+        elem = document.getElementById("myBar");
+        width = 1;
+        id = setInterval(this.frame, 10);
+        elem.style.backgroundColor = "#2732ce";
+      }
+    },
+    frame() {
+      if (width >= 100 || !timer) {
+        clearInterval(id);
+        j = 0;
+        if (width >= 100) {
+          this.answer();
+          this.clicked = true;
+          this.nextbtn = "block";
+          timer = 0;
+          this.livesleft();
+  
+        }
+      } else if (timer) {
+        width += 0.2;
+        elem.style.width = width + "%";
+        if (width >= 55) {
+          elem.style.backgroundColor = "#dcdf35";
+        }
+        if (width >= 80) {
+          elem.style.backgroundColor = "#cf2e2e";
+        }
+      }
+    },
+    livesleft() {
       this.livesstyle[0] = "rgb(218, 218, 218)";
       this.livesstyle[1] = "lost 600ms infinite";
       this.lives--;
@@ -90,26 +127,23 @@ const app = Vue.createApp({
       if (this.lives <= 0) {
         this.nextbtn = "none";
         alert(
-          "Game Over \n Score : " +
-            this.score +
-            "\n Better Luck Next Time :)")
-        }
-
+          "Game Over \n Score : " + this.score + "\n Better Luck Next Time :)"
+        );
+      }
     },
     result(optelement, index) {
+      timer = 0;
       if (!this.clicked && this.lives > 0) {
         this.clicked = true;
         this.nextbtn = "block";
         if (document.getElementById(optelement).innerHTML == this.correctopt) {
           this.resultstyle[index] = "rgb(5, 151, 5)";
           this.score++;
-        }
-        else {
+        } else {
           this.resultstyle[index] = "rgba(212, 33, 33, 0.959)";
-          this.livesleft()
-          }
-          this.answer();
-        
+          this.livesleft();
+        }
+        this.answer();
       }
     },
     opt1clicked() {
